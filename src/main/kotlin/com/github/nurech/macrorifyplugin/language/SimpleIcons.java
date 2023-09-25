5265e2046465072370e1e0de6646d281080c645f
Joosep Parts <joosep.parts@gmail.com>
2023-09-25 19:39:04 +0300

    init

---
+++ src/main/kotlin/com/github/nurech/macrorifyplugin/language/SimpleIcons.java
@@ -0,0 +1,9 @@
+package com.github.nurech.macrorifyplugin.language;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+public class SimpleIcons {
+    public static final Icon FILE = IconLoader.getIcon("/icons/icon.png", SimpleIcons.class);
+}
